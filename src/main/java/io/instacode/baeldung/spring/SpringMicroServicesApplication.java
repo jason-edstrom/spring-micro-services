553d350b34318fd9aa775c666ad39e59fb0b1a37
Jason Edstrom <jason@instacode.io>
2021-06-12 17:20:51 -0500

    initial commit

---
+++ src/main/java/io/instacode/baeldung/spring/SpringMicroServicesApplication.java
@@ -0,0 +1,13 @@
+package io.instacode.baeldung.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroServicesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringMicroServicesApplication.class, args);
+  }
+
+}
